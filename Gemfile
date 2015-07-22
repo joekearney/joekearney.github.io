63c5cdc36fdd0ee76422e0feee3b330b92370b3f
Joe Kearney <mail@joekearney.co.uk>
2015-07-22 12:14:35 +0100

    ensure that everything is installed when running

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
-ruby '2.0.0'
+ruby "2.0.0"
 
+# github-pages pulls in jekyll, among others
 gem 'github-pages'
 gem 'rake'
 gem 'rmagick'
--- runLocal.sh
+++ runLocal.sh
@@ -3,4 +3,8 @@
 SCRIPT=$(readlink -f $0)
 DIR=$(dirname $SCRIPT)
 
+echo "Ensuring that the required Ruby gems are installed..."
+$(cd $DIR; bundle install --quiet)
+echo "Done with exit code $?"
+
 jekyll serve -s $DIR -w
