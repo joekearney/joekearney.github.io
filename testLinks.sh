925117e12a77d6c981e8d4dc716790e60df11e7d
Joe Kearney <jjk@soundcloud.com>
2016-04-12 09:33:42 +0200

    linkchecker testing

--- testLinks.sh
+++ testLinks.sh
@@ -1,1 +1,12 @@
-linkchecker http://localhost:4000 --check-extern
+#!/bin/bash
+
+if [ "$1" == "local" ]; then
+  TARGET="http://localhost:4000"
+elif [[ "$1" == "prod" ]]; then
+  TARGET="http://www.joekearney.co.uk"
+else
+  echo "Usage: $0 <local|prod>"
+  exit 1
+fi
+
+linkchecker $TARGET --check-extern
