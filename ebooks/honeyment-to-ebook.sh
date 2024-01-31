27de4046201884d55e860d66fcdcdfadb5639b58
Joe Kearney <mail@joekearney.co.uk>
2024-01-31 21:35:26 +0100

    fix up ebook script

--- ebooks/honeyment-to-ebook.sh
+++ ebooks/honeyment-to-ebook.sh
@@ -1,24 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-DIR=$(readlink -f $0 | xargs dirname)
+DIR=$(readlink -f "$0" | xargs dirname)
 outputFile=$DIR/honeyment
 configFile=$DIR/honeyment.yaml
 coverImageFile=$DIR/../images/honeyment-cover.jpg
 
-files=($(grep -l "layout: post" $DIR/../_posts/honeyment/*.md | xargs grep -l "published: true" | sort))
+mapfile -t files < <(grep -l "layout: post" "$DIR"/../_posts/honeyment/*.md | \
+  xargs grep -l "published: true" | \
+  sort \
+)
+
+# files=($(grep -l "layout: post" $DIR/../_posts/honeyment/*.md | xargs grep -l "published: true" | sort))
 
 tmpfile=$(mktemp /tmp/honeyment-ebook.md.XXXXXX)
 trap 'rm -f -- "$tmpfile"' INT TERM HUP EXIT
+touch "$tmpfile"
 
-touch $tmpfile
-
-cat $DIR/../_posts/honeyment/honeyment-intro.md | sed 's/### In /# In /' >> $tmpfile
+cat "$DIR/../_posts/honeyment/honeyment-intro.md" | \
+  sed 's/### In /# In /' >> "$tmpfile"
 
 # for each file    | trim out the front-matter
-echo "${files[@]}" | \
-  xargs -n 1 $DIR/trim-md.sh >> $tmpfile
+for f in "${files[@]}"; do
+  echo "$f"
+  "$DIR/trim-md.sh" "$f" >> "$tmpfile"
+done
+# echo "${files[@]}" | \
+#   xargs -n 1 "$DIR/trim-md.sh" >> "$tmpfile"
 
-pandoc -S -s --toc-depth=1 -o $outputFile.epub --epub-cover-image=$coverImageFile $configFile $tmpfile
-kindlegen $outputFile.epub
+echo "Calling pandoc..."
+pandoc -s --toc-depth=1 -o "$outputFile.epub" --epub-cover-image="$coverImageFile" "$configFile" "$tmpfile"
+# kindlegen $outputFile.epub
--- ebooks/trim-md.sh
+++ ebooks/trim-md.sh
@@ -1,16 +1,54 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
+function echoErr() {
+  cat <<< "$@" 1>&2
+}
+
+echoErr "Starting trim"
+
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <filename>"
-  echo "Trims out the yaml front matter, adds '# <Title>', then prints the content"
+  echoErr "Usage: $0 <filename>"
+  echoErr "Trims out the yaml front matter, adds '# <Title>', then prints the content"
   exit 1
 fi
 
 file=$1
-title=$(egrep 'title: .*' $file | sed -r 's/^title: "?([^"]*)"?$/\1/')
-subtitle=$(egrep 'description: .*' $file | sed -r 's/^description: "?([^"]*)"?$/\1/')
+echoErr "Trimming: $file"
+basefile=$(basename "$file")
+
+function extract() {
+  local field="$1"
+  echoErr "    Looking for field: $field"
+  yq --front-matter=extract ".$field" < "$file"
+}
+
+function extract_array() {
+  local field="$1"
+  local join="$2"
+  echoErr "    Looking for field: $field"
+  yq --front-matter=extract ".$field | join(\"$join\")" < "$file"
+}
+
+# title=$(grep -e 'title: .*' "$file" | sed -r 's/^title: "?([^"]*)"?$/\1/')
+# subtitle=$(grep -e 'description: .*' "$file" | sed -r 's/^description: "?([^"]*)"?$/\1/')
+# author=$(grep -e 'author: .*' "$file" | sed -r 's/^author: "?([^"]*)"?$/\1/')
+
+title=
+subtitle=
+author=
+
+title="$(extract "title")"
+subtitle="$(extract "description")"
+author="$(extract_array "author" ", ")"
+
+echoErr "  title[$title] -- subtitle[$subtitle]"
+echoErr "  author[$author]"
+
+# if [[ "$author" == "" ]]; then
+  # exit 1
+# fi
 
 function toPrettyDate() {
   local fileWithDate=$1
@@ -21,25 +59,29 @@ function toPrettyDate() {
   echo "$dd ${MONTHS[$mm]} $yyyy"
 }
 
-basefile=$(basename $file)
-prettyDate=$(toPrettyDate $basefile)
+prettyDate=$(toPrettyDate "$basefile")
+dateAuthor="$prettyDate -- $author"
 if [[ "$subtitle" != "" ]]; then
   byline="$prettyDate -- $subtitle"
 else
   byline="$prettyDate"
 fi
 
+echoErr "  trimming..."
+
 echo
 # remove front matter
-# add chapter title, subtitle
+# add chapter title, subtitle -- \n\n## ${author}
 # remove HTML blocks
 # remove liquid/jekyll blocks
 # remove hyperlinks
 # remove [^akaroa] links
-awk '/---/{p++}p==2' $file | \
-  sed "s/---/# $title\n\n## ${byline}/" | \
+awk '/---/{p++}p==2' "$file" | \
+  sed "s/---/# $title\n\n### ${byline}\n\n#### ${author}/" | \
   sed '/^</,/^<\//d' | \
   sed -r 's/^(\{%.*|.*%\})$//' | \
-  sed -r 's/\[([^]]*)\]\(.*\)/\1/g' | \
+  sed -r 's/\[([^]]*)\]\([^)]*\)/\1/g' | \
   sed 's/\(.\+\)\[^akaroa\]/\1/g'
 echo
+
+echoErr "  finished"
